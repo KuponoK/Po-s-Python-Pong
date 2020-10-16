ba7b814b8843192f808f5dfe2d00be88e3d04e85
Kupono_Kealiinohomoku <kupono.kealiinohomoku@gmail.com>
2020-10-16 13:06:50 -0700

    paddle setup

--- pong.py
+++ pong.py
@@ -10,6 +10,25 @@ win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
 
+# Player 1 Paddle
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350, 0)
+
+# Player 2 Paddle
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350, 0)
+
+
 # Main Game Loop
 while True:
     win.update()
