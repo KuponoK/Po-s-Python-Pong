ba311b4c203fb927c8ad9c31f41dbbbb3dfb161f
Kupono_Kealiinohomoku <kupono.kealiinohomoku@gmail.com>
2020-10-16 12:53:58 -0700

    screen setup
    Got the screen layout setup

--- pong.py
+++ pong.py
@@ -1,4 +1,15 @@
-#Welcome to Po's Python Pong Game
-#By Po Kealiinohomoku
-#Intensive Project 1
+# Welcome to Po's Python Pong Game
+# By Po Kealiinohomoku
+# Intensive Project 1
 
+import turtle
+
+win = turtle.Screen()
+win.title("Po's Python Pong")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Main Game Loop
+while True:
+    win.update()
