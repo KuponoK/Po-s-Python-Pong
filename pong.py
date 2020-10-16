233ce27b07633cd8f373dc1d0032ed317fa96abc
Kupono_Kealiinohomoku <kupono.kealiinohomoku@gmail.com>
2020-10-16 13:48:15 -0700

    Game ball setup
    + cleaned up some of the code and added comments

--- pong.py
+++ pong.py
@@ -4,11 +4,14 @@
 
 import turtle
 
+# Screen setup
 win = turtle.Screen()
 win.title("Po's Python Pong")
 win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
+
+# Gameplay objects
 
 # Player 1 Paddle
 paddle_1 = turtle.Turtle()
@@ -28,6 +31,14 @@ paddle_2.shapesize(stretch_wid=5, stretch_len=1)
 paddle_2.penup()
 paddle_2.goto(350, 0)
 
+# Game ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.forward(100)
 
 # Main Game Loop
 while True:
